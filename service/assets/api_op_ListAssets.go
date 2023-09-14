ff5bac5429b629e37aea73043334e430f59250bb
Nabil Houidi <35373676+NabilHouidi@users.noreply.github.com>
2023-09-14 18:53:26 +0100

    feat: update assets to EDC v0.2.0

--- service/assets/api_op_ListAssets.go
+++ service/assets/api_op_ListAssets.go
@@ -6,29 +6,36 @@ import (
 
 	"github.com/Think-iT-Labs/edc-connector-client-go/common/apivalidator"
 	"github.com/Think-iT-Labs/edc-connector-client-go/internal"
+	"github.com/Think-iT-Labs/edc-connector-client-go/internal/sharedtypes"
 )
 
+type QueryInputPayload struct {
+	apivalidator.QueryInput
+	sharedtypes.BaseRequest
+}
+
 func (c *Client) ListAssets(args ...apivalidator.QueryInput) ([]AssetProperties, error) {
-	var queryInput apivalidator.QueryInput
+	var queryInputPayload *QueryInputPayload
 
 	if len(args) > 0 {
-		queryInput = args[0]
-		err := apivalidator.ValidateQueryInput(args[0].SortOrder)
+		queryInputPayload = &QueryInputPayload{}
+		queryInputPayload.QueryInput = args[0]
+		err := apivalidator.ValidateQueryInput(queryInputPayload.SortOrder)
 		if err != nil {
 			return nil, err
 		}
+		queryInputPayload.Context = sharedtypes.EdcContext
 	} else {
-		queryInput = apivalidator.QueryInput{}
+		queryInputPayload = nil
 	}
 
 	endpoint := fmt.Sprintf("%s/assets/request", *c.Addresses.Management)
 	assets := []AssetProperties{}
-
 	err := c.HTTPClient.InvokeOperation(internal.InvokeHTTPOperationOptions{
 		Method:             http.MethodPost,
 		Endpoint:           endpoint,
-		RequestPayload:     queryInput,
-		ResponsePayload:    assets,
+		RequestPayload:     queryInputPayload,
+		ResponsePayload:    &assets,
 		ExpectedStatusCode: http.StatusOK,
 	})
 	if err != nil {
